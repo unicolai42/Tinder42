445e17fdb022468589ded583044491adbb5c241f
Ugo Nicolai <unicolai@student.42.fr>
2018-09-16 22:46:35 +0200

    fix script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ require("isomorphic-fetch")
 const mysql = require('mysql')
 const fs = require('fs')
 const bcrypt = require('bcrypt')
+const randomString = require('randomstring')
 
 
 const db = mysql.createConnection({
@@ -16,7 +17,7 @@ const sqlFile = fs.readFileSync('database_tinder42.sql').toString()
 
 
 db.connect(function(err) {
-    if (err) throw err;
+    if (err) throw err
     console.log("Connected!");
     db.query(sqlFile, (err, result) => {
         if (err) throw err
@@ -25,35 +26,39 @@ db.connect(function(err) {
     fetch('https://randomuser.me/api/?results=5&nat=fr')
     .then(response => response.json())
     .then(data => {
-        // console.log(data.results)
-        data.results.forEach(elem => {
+        const users = data.results
+        for (let i = 0; i < users.length; i++) {
+            let elem = users[i]        
             const saltRounds = 10;
             const password = elem.login.password
             let sex = (elem.gender === 'male') ? 0 : 2
         
             bcrypt.hash(password, saltRounds, function(err, hashPassword) {
                 const randomKey = randomString.generate(15)
-                req.db.query(`INSERT INTO Users (username, password, mail, randomKey, picture1, age, sex) VALUES (?, ?, ?, ?, ?);`,
+                db.query(`INSERT INTO Users (username, password, mail, randomKey, picture1, age, sex) VALUES (?, ?, ?, ?, ?, ?, ?);`,
                 [elem.login.username, hashPassword, elem.email, randomKey, elem.picture.medium, elem.dob.age, sex], (err, rows, fields) => {
                     if(err)
-                        return(res.send(err) && console.log(err))
+                        return(console.log(err))
                     
-                    req.db.query(`SELECT * FROM Users WHERE username = ?;`,
+                    db.query(`SELECT * FROM Users WHERE username = ?;`,
                     [elem.login.username], (err, rows, fields) => {
                         if(err)
-                            return(res.send(err) && console.log(err));
+                            return(console.log(err))
+
                         const userId = rows[0].id
-                        req.db.query(`INSERT INTO Preferences (age_min, age_max, max_distance, sex, user_id) VALUES (16, 70, 500000, 1, ?);`,
+                        db.query(`INSERT INTO Preferences (age_min, age_max, max_distance, sex, user_id) VALUES (16, 70, 500000, 1, ?);`,
                         [userId], (err, rows, fields) => {
                             if(err)
-                                return(res.send(err) && console.log(err))
+                                return(console.log(err))
+                                
+                            if (i === data.results.length - 1)
+                                db.end()
                         })
                     })
                 })
             })
-        })
+        }
     })
-    db.end()
 });
 
 
